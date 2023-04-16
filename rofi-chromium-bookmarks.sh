1de98ca3860f44e80a7a1a68011f1382af7f128b
Giovanni Campeol <giovanni@smartpricing.it>
2023-04-16 08:31:34 +0200

    first commit

---
+++ rofi-chromium-bookmarks.sh
@@ -0,0 +1,18 @@
+#1/bin/bash
+
+script_path=$(realpath "$0")
+script_name=$(echo $0 | rev | cut -d "/" -f1 | rev)
+script_dir="${script_path/$script_name/""}"
+theme="${script_dir}theme.rasi"
+
+bookmarks_path="$HOME/.config/chromium/Default/Bookmarks"
+
+slected_bookmark=$(jq '.roots.bookmark_bar.children | map(.name)' $bookmarks_path | jq -r -c '.[]' | sort -r | rofi -dmenu -i -theme $theme -w 100 -p "Chroimum Bookmarks:")
+if [[ "${slected_bookmark}" == "" ]]; then exit; fi
+echo $slected_bookmark
+
+selected_bookmark_url=$(jq -rc ".roots.bookmark_bar.children[] | select(.name==\"${slected_bookmark}\") | .url" $bookmarks_path)
+if [[ "$selected_bookmark_url" == "" ]]; then exit; fi
+echo $selected_bookmark_url
+
+chromium $selected_bookmark_url;
---
+++ rofi-path-launcher.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+ADD_OPTION="+"
+REMOVE_OPTION="-"
+
+script_path=$(realpath "$0")
+script_name=$(echo $0 | rev | cut -d "/" -f1 | rev)
+script_dir="${script_path/$script_name/""}"
+theme="${script_dir}theme.rasi"
+
+## get config from options
+config=""
+for (( i=1; i <= "$#"; i++ )); do
+	case ${!i} in "-c" | "--config")
+		value=$(($i + 1))
+		echo $value
+		config="${!value}"
+		echo $config
+		shift
+	esac
+done
+
+## if no config in options, use default 
+if [[ "$config" == "" ]]; then 
+	config_path="${HOME}/.1launcher.config.json"
+	if [[ ! -f $config_path ]]; then 
+		touch $config_path
+		echo "{}" > $config_path
+	fi
+	config=$(cat $config_path)
+fi
+
+function get_program {
+	programs_num=$({ echo $config | jq '. |= keys' | jq -r -c '.[]'; echo $ADD_OPTION; echo $REMOVE_OPTION; } | wc -l) 
+	if [[ $programs_num -gt 10 ]]; then programs_num=10; fi
+	program=$({ echo $config | jq '. |= keys' | jq -r -c '.[]'; echo $ADD_OPTION; echo $REMOVE_OPTION; } | rofi -dmenu -i -theme $theme -l $programs_num -p "1launcher:")
+	if [[ $program == $ADD_OPTION ]]; then add_program;
+	elif [[ $program == $REMOVE_OPTION ]]; then remove_program;
+	elif [[ $program != "" ]]; then get_program_path $program; 
+	fi
+}
+
+function get_program_path {
+	program="$1"
+	
+	pattern=".${program}.paths[]"
+	lines_num=$({ echo $config | jq -r -c $pattern | sort; echo $ADD_OPTION; echo $REMOVE_OPTION; } | wc -l)
+	if [[ $lines_num -gt 10 ]]; then lines_num=10; fi
+	path=$({ echo $config | jq -r -c $pattern | sort; echo $ADD_OPTION; echo $REMOVE_OPTION; }  | rofi -dmenu -i -theme $theme -l $lines_num -p "$program (path)")
+
+	if [[ $path == "" ]]; then get_program;
+	elif [[ $path == $ADD_OPTION ]]; then add_path_to_program $program;
+	elif [[ $path == $REMOVE_OPTION ]]; then remove_path_from_program $program;
+	else execute_command $1 $path;
+	fi
+}
+
+function execute_command {
+	program="$1"
+	path="$2"
+
+	pattern=".${program}.command"
+	command=$(echo $config | jq -r $pattern)
+	
+	command="${command/"<path>"/$path}"
+	eval $command
+}
+
+function add_path_to_program {
+	program="$1"
+
+	path=$(echo "" | rofi -dmenu -i -theme $theme -l 0 -p "$program (add path)")
+
+	if [[ "$path" == "" ]]
+		then get_program_path $program
+		else
+			abs_path=`readlink -f "$path"`
+			if [ -d $abs_path ] || [ -f $abs_path ]; then
+				pattern=".${program}.paths + [\"${path}\"]"
+				new_paths=$(jq "$pattern" $config_path)
+
+				pattern=".${program}.paths = ${new_paths}"
+				new_config=$(jq "$pattern" $config_path)
+
+				echo $new_config > $config_path
+				config=$(cat $config_path);
+			else echo "path not valid"; fi
+
+			get_program_path $program
+	fi
+}
+
+function remove_path_from_program {
+	program="$1"
+
+	lines_num=$(echo $config | jq -r -c $pattern | wc -l)
+	if [[ $lines_num -gt 10 ]]; then lines_num=10; fi
+	path=$(echo $config | jq -r -c $pattern | rofi -dmenu -i -theme $theme -l $lines_num -p "$program (remove path)")
+
+	if [[ "$path" != "" ]]; then
+		pattern=".${program}.paths - [\"${path}\"]"
+		new_paths=$(jq "$pattern" $config_path)
+
+		pattern=".${program}.paths = ${new_paths}"
+		new_config=$(jq "$pattern" $config_path)
+
+		echo $new_config > $config_path
+		config=$(cat $config_path)
+	fi
+
+	get_program_path $1
+}
+
+function add_program {
+	program_name=$(echo "" | rofi -dmenu -i -theme $theme -l 0 -p "1launcher (program name)")
+
+	if [[ "$program_name" == "" ]]; then get_program;
+	else 
+		program_command=$(echo "" | rofi -dmenu -i -theme $theme -l 0 -p "1launcher (program cmd)")
+
+		pattern=". + {\"${program_name}\":{\"command\":\"${program_command}\",\"paths\":[]}}"
+		new_config=$(jq "$pattern" $config_path)
+
+		echo $new_config > $config_path
+		config=$(cat $config_path)
+
+		get_program;
+	fi
+}
+
+function remove_program {
+	programs_num=$(echo $config | jq '. |= keys' | jq -r -c '.[]' | wc -l) 
+	if [[ $programs_num -gt 10 ]]; then programs_num=10; fi
+	program=$(echo $config | jq '. |= keys' | jq -r -c '.[]' | rofi -dmenu -i -theme $theme -l $programs_num -p "1launcher:")
+
+	pattern="del(.$program)"
+	new_config=$(jq "$pattern" $config_path)
+	
+	echo $new_config > $config_path
+	config=$(cat $config_path)
+
+	get_program
+}
+
+get_program
