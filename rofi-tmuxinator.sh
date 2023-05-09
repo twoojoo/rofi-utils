ba4d5afc43b7b33b57f017ec7a07090422c12c9f
Giovanni Campeol <giovanni@smartpricing.it>
2023-05-09 15:02:00 +0200

    added medium theme

--- rofi-tmuxinator.sh
+++ rofi-tmuxinator.sh
@@ -3,7 +3,7 @@
 script_path=$(realpath "$0")
 script_name=$(echo $0 | rev | cut -d "/" -f1 | rev)
 script_dir="${script_path/$script_name/""}"
-theme="${script_dir}theme.rasi"
+theme="${script_dir}theme-medium.rasi"
 
 function print_error {
 	rofi -e "Error: $1" 
